de459ec67c0520e2dca5c21325813c00fbbda263
Ocrezzu <davidjrf95@gmail.com>
2021-01-30 12:11:21 -0500

    Update sample_spec.js

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,9 +1,12 @@
 describe('My first Cypress Test',()=>{
     it('Mi tienda y sus Test',()=>{
         cy.visit('http://localhost:8080/')
-        cy.get('form-control form-control-lg').type('iPhone {enter}')
-        cy.contains('iPad').click( )
+        cy.get('.jumbotron').get('form')
+        cy.get('.form-control form-control-lg').get('input').type('iPhone {enter}')
+        cy.wait(3000)
 
+        cy.contains('iPhone').get('.btn-outline-success:first').click();
+        cy.url().should('include','/producto');//ASSERT
      })
      
  })
